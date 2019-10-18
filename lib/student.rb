ee113697877b5dc595acc9178e2e03225b98c320
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-18 15:21:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,15 @@
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  #  with DB[:conn]
+  
+  attr_accessor :name, :grade, :id
+  
+  def initialize(name, grade, id = nil)
+    @name = name 
+    @grade = grade 
+  end 
+  
+  
   
 end
