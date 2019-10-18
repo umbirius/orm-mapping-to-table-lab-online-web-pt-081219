b190cb5c877d8e582c823c62a5b4f649ec5e70f1
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-18 15:27:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,7 +24,10 @@ class Student
   
   def self.drop_table
     sql = <<-SQL
-      DROP TABLE students
+      (DROP TABLE IF EXISTS students)
+      SQL 
+    DB[:conn].execute(sql)
+  end 
   
   
   
