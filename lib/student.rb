7394b8e8d8ac33366687022d6a2f4a396176637d
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-18 15:24:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,9 +11,17 @@ class Student
     @grade = grade 
   end 
   
-  def self.table 
-    
+  def self.create_table 
+    sql = <<-SQL
+      CREAT TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT, 
+        grade INTEGER
+        )
+        SQL
+    DB[:conn].execute(sql)
   end 
+  
   
   
 end
