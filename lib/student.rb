1729a78e5b007dcb30c6d1c095d2a9d9b7998741
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-18 15:31:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,9 +30,9 @@ class Student
   end 
   
   def save 
-    sql = <<- SQL 
+    sql = <<-SQL 
       INSERT INTO students (name, grade)
-      VALUES (?, ?)
+      VALUES (?, ?) 
     SQL
     
     DB[:conn].execute(sql, self.name, self.grade)
